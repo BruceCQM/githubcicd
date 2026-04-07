62f3aac0e6a5d94b91b3e88b09c1dabcc84badcf
Bruce <666@gmail.com>
2026-04-07 23:58:41 +0800

    feat: 注释服务器访问异常的配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/githubcicd/',
-  build: {
-    assetsInlineLimit: 0
-  }
+  // base: '/githubcicd/',
+  // build: {
+  //   assetsInlineLimit: 0
+  // }
 })
